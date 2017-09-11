32ed2320362553637416dd185505601c38eb59b9
talanovartem <talanovartem@gmail.com>
2017-09-12 01:09:49 +0300

    111

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -10,6 +10,8 @@ var Controls = React.createClass({
       this.props.onStatusChange(newStatus);
     }
   },
+
+
   render: function () {
     var {countdownStatus} = this.props;
     var renderStartStopButton = () => {
